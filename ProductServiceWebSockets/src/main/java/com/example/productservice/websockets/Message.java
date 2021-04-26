4a42d67c9a39db5f70a99efb04aaf50fdb677f7a
guiller0300 <guiller0300@gmail.com>
2021-04-26 14:43:22 -0500

    a

---
+++ ProductServiceWebSockets/src/main/java/com/example/productservice/websockets/Message.java
@@ -0,0 +1,15 @@
+package com.example.productservice.websockets;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
---
+++ ProductServiceWebSockets/src/main/java/com/example/productservice/websockets/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.productservice.websockets;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Controller
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(final MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(final StompEndpointRegistry registry) {
+		registry.addEndpoint("/chat");
+		registry.addEndpoint("/chat").withSockJS();
+		registry.addEndpoint("/chatwithbots");
+		registry.addEndpoint("/chatwithbots").withSockJS();
+	}
+}
